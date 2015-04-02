2644d22e7af7cb0348ecfb36d4e1afe86b8876d6
likandia <likandia@gmail.com>
2015-04-02 22:21:46 +0530

    Add __main__ module

--- readsound.py
+++ readsound.py
@@ -3,24 +3,26 @@ import numpy
 import matplotlib.pyplot as plt
 
 
-RATE = 48000
-CHANNELS = 1
-FRAMES = 3072
-FORMAT = pyaudio.paInt16
+if __name__ == "__main__":
 
-recording_time = 10
+    RATE = 48000
+    CHANNELS = 1
+    FRAMES = 3072
+    FORMAT = pyaudio.paInt16
+
+    recording_time = 10
 
 
-p = pyaudio.PyAudio();
-s = p.open(rate = RATE,
-           channels = CHANNELS,
-           format = FORMAT,
-           frames_per_buffer = FRAMES,
-           input = True)
+    p = pyaudio.PyAudio();
+    s = p.open(rate = RATE,
+               channels = CHANNELS,
+               format = FORMAT,
+               frames_per_buffer = FRAMES,
+               input = True)
 
-data = s.read(FRAMES)
-data = numpy.fromstring(data,dtype=numpy.int16)
-plt.plot(data)
-plt.show()
+    data = s.read(FRAMES)
+    data = numpy.fromstring(data,dtype=numpy.int16)
+    plt.plot(data)
+    plt.show()
 
-s.close()
+    s.close()
