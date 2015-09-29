757ac1da2ec93543e56ff71b809a874fb0e9840a
P.C. Sruthi <likandia@gmail.com>
2015-09-29 13:48:02 +0530

    Delete unnecessary commented statements

--- realtime_wani.py
+++ realtime_wani.py
@@ -46,9 +46,6 @@ def callback(in_data, frame_count, time_info, status):
     wf.writeframes(in_data)
 
     curve.setData(final[-20000:])
-    #w1.plot(final[-3000:])
-    #plt.ylim(0.0, 50000.0)
-    #plt.pause(0.0001)
     print final
     return (frames, pyaudio.paContinue)
 
@@ -56,7 +53,6 @@ def getaudio():
     global final
     frames = ''
     p = pyaudio.PyAudio()
-    #for i in range(0, int(RATE / (CHUNK) * RECORD_SECONDS)):
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE, #sampling rate
@@ -64,24 +60,6 @@ def getaudio():
                     frames_per_buffer=CHUNK,
                     stream_callback=callback)
     stream.start_stream()
-    #time.sleep(RECORD_SECONDS)
-
-    #data = stream.read(CHUNK)
-    #frames += data
-    #final = numpy.fromstring(frames, dtype=numpy.int16)
-    #final = abs(final)
-    #final = final - 500
-    #masked = numpy.ma.masked_where(final < 0, final)
-    #masked.fill_value = 0
-    #final = numpy.ma.filled(masked)
-
-    #plt.plot(final[-3000:], 'b')
-
-    #stream.stop_stream()
-    #stream.close()
-    #time.sleep(0.0001)
-    #print final
-    #curve.set_data(final[-3000:])
     stream.stop_stream()
     stream.close()
     p.terminate()
@@ -91,22 +69,11 @@ def main():
     frames = ''
     final = numpy.fromstring(frames, dtype=numpy.int16)
 
-    #fig = plt.figure()
-    #ax = plt.axes(xlim=(0.0, 2000.0), ylim=(0.0, 50000.0))
-    #line, = ax.plot([], [], lw=2)
-
-#timer = QtCore.QTimer()
-#timer.timeout.connect(getaudio)
-#timer.start(RECORD_SECONDS)
-
-
-
 if __name__ == "__main__":
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
             timer = QtCore.QTimer()
             timer.timeout.connect(getaudio)
             timer.start(1)
-            #time.sleep(1)
 
             pg.QtGui.QApplication.instance().exec_()
